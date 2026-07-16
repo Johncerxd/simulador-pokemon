790335918ddd85bc11418c41bd1f7e8daf9ba10b
Johncerxd <140997545+Johncerxd@users.noreply.github.com>
2026-07-16 18:17:36 -0500

    Fix header formatting in Persistencia section

--- app.py
+++ app.py
@@ -674,8 +674,7 @@ elif st.session_state.pagina == "Historial":
         st.info("Aun no se han registrado acciones.")
 
 elif st.session_state.pagina == "Persistencia":
-    st.header("💾 Creador de entrenador
-    ")
+    st.header("💾 Creador de entrenador")
 
     col1, col2 = st.columns(2)
     if col1.button("💿 Guardar partida"):
